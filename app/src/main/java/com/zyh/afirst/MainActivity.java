102485a7759a304400a9e5a217d651b210a7898a
xuezayun <xuezayun@126.com>
2015-08-23 21:02:39 +0800

    Rejected commit: Default

--- app/src/main/java/com/zyh/afirst/MainActivity.java
+++ app/src/main/java/com/zyh/afirst/MainActivity.java
@@ -4,14 +4,24 @@ package com.zyh.afirst;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.zyh.afirst.utilities.MyTextUtil;
 
 public class MainActivity extends Activity {
-    int i = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv_showtext);
+        showValue(tv);
+
+    }
+
+    private void showValue(TextView tv) {
+        MyTextUtil myUtil = new MyTextUtil();
+        tv.setText(myUtil.getValue());
     }
 
     @Override
---
+++ app/src/main/java/com/zyh/afirst/utilities/MyTextUtil.java
@@ -0,0 +1,11 @@
+package com.zyh.afirst.utilities;
+
+/**
+ * Created by Administrator on 2015/8/23.
+ */
+public class MyTextUtil {
+    public String getValue() {
+        return "Test for AS";
+    }
+
+}
